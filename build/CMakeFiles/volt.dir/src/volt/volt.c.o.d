8e01b07359ac5daa841bc1a0af45baf1b27292a4
Chase Sunstrom <casunstrom+github@gmail.com>
2025-10-02 09:16:49 -0500

    Update

---
+++ build/CMakeFiles/volt.dir/src/volt/volt.c.o.d
@@ -0,0 +1,8 @@
+CMakeFiles/volt.dir/src/volt/volt.c.o: \
+ /home/chase/Projects/C/Volt/src/volt/volt.c /usr/include/stdc-predef.h \
+ /home/chase/Projects/C/Volt/include/pch.h \
+ /home/chase/Projects/C/Volt/include/volt/volt.h \
+ /home/chase/Projects/C/Volt/include/volt/error.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/include/stdint.h \
+ /home/chase/Projects/C/Volt/include/util/types.h \
+ /home/chase/Projects/C/Volt/include/util/fmt.h
