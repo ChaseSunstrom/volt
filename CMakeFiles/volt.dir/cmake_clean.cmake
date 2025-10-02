624e517a7b2187394347318af193a33ee502a866
Chase Sunstrom <casunstrom+github@gmail.com>
2025-10-02 09:21:13 -0500

    Update

---
+++ CMakeFiles/volt.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/volt.dir/link.d"
+  "CMakeFiles/volt.dir/cmake_pch.h.gch"
+  "CMakeFiles/volt.dir/cmake_pch.h.gch.d"
+  "CMakeFiles/volt.dir/src/main.c.o"
+  "CMakeFiles/volt.dir/src/main.c.o.d"
+  "CMakeFiles/volt.dir/src/util/fmt.c.o"
+  "CMakeFiles/volt.dir/src/util/fmt.c.o.d"
+  "CMakeFiles/volt.dir/src/volt/volt.c.o"
+  "CMakeFiles/volt.dir/src/volt/volt.c.o.d"
+  "volt"
+  "volt.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/volt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
